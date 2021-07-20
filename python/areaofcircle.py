4fa81d21046e40da49adb27476b579494726f394
Ram Cheekoti <ramch011@gmail.com>
2021-07-20 11:24:26 +0530

    added the files

--- python/areaofcircle.py
+++ python/areaofcircle.py
@@ -1,3 +1,4 @@
+#Area of Circle
 pi=3.14
 r=float(input("Enter the radius of the cricle: "))
 print("The Radius of the Criclet is :", r)
--- python/complexnumber.py
+++ python/complexnumber.py
@@ -1,4 +1,4 @@
-comp1 = complex(input("Enter the complex number 1"))
-comp2 = complex(input("Enter the complex number 2"))
+comp1 = complex(input("Enter the complex number 1 \n"))
+comp2 = complex(input("Enter the complex number 2 \n"))
 comp3 = comp1+comp2
 print(comp1,comp2,comp3)
